5c22c0f0cad3ae18e5afdd5a8c4aec9ec2c73ff8
techwensananda <ananda.gharami@techwens.in>
2024-02-04 18:41:49 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import OtpInput from './components/PhoneLogin'
+// import PhoneLogin from './components/PhoneLogin'
+
+function App() {
+
+  return (
+    <div className='App'>
+     <h1>Login with Otp</h1>
+     <OtpInput />
+    </div>
+  )
+}
+
+export default App
